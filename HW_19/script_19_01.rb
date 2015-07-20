f3e9eb455dfb87a4d8d68cbd0fcf299b4ee56cac
saschik1990 <saschik1990@gmail.com>
2015-07-20 13:44:26 -0700

    First commit

---
+++ HW_19/script_19_01.rb
@@ -0,0 +1,12 @@
+recuire "selenium-webdriver"
+
+browser = SELENIUM::WEBDRIVER.for :firefox
+browser.get "http://learn2test.net/qa/apps/sign_up/v1/"
+
+#title validation
+puts "Title is: #{browser.title}"
+
+#quotes validation
+quotes = browser.find_elemnt(:id, "id_quotes")
+puts "Quote is dysplayed: #{quotes.displayed?}"
+puts "Quote is: #{quotes.to_s}"
